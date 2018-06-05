0babcf01204180b73692d057ada7cd059dfba5e7
Gar <gar@lonelydatu.com>
2018-06-05 10:08:24 -0400

    one

--- dev/passion_300x250/_js/main.js
+++ dev/passion_300x250/_js/main.js
@@ -2,49 +2,63 @@ import {size, slideIn, slideOut, init} from '../../_common/js/common.js'
 
 
 const el = {
-	t1a:{isLeft: true},
-	t1b:{isLeft: true},	
-	t2a:{isLeft: true},
-	t2b:{isLeft: true}	
+	t1a:{isLeft: false},
+	t1b:{isLeft: true},
+	t1c:{isLeft: false},
+	t2a:{isLeft: false},
+	t2b:{isLeft: true},	
+	t3a:{isLeft: true},
+	t3b:{isLeft: false},
+	t3c:{isLeft: false}
 }
+
+
 
 
 
 function start() {
 	const tl = new TimelineMax()
 	
-	TweenLite.to('#playa', 8, {ease:Power3.easeOut, x:-44})
+	
+	TweenLite.to('#playa', 10, {ease:Power3.easeOut, x:-129, y:-88})
 	
 	tl.add('f1')	
 	tl.set('.frame1', {opacity: 1})
 
 	tl.add( slideIn(el.t1a) )
-	tl.add( slideIn(el.t1b), '-=.2' )	
+	tl.add( slideIn(el.t1b), '-=.2' )
+	tl.add( slideIn(el.t1c), '+=.1' )
 	
 	tl.add('f1_end', "+=1.5")
 	tl.add( slideOut(el.t1a), 'f1_end' )
-	tl.add( slideOut(el.t1b), 'f1_end-=.1' )	
+	tl.add( slideOut(el.t1b), 'f1_end-=.1' )
+	tl.add( slideOut(el.t1c), 'f1_end-=.2' )
 
 	tl.add('f2')
 	tl.set('.frame2', {opacity: 1}, 'f2')
 	tl.add( slideIn(el.t2a) )
 	tl.add( slideIn(el.t2b), '-=.2' )
-
+	// tl.add( slideIn(el.t2c), '-=.2' )
 
 	tl.add('f2_end', "+=1.5")
 	tl.add( slideOut(el.t2a), 'f2_end' )
 	tl.add( slideOut(el.t2b), 'f2_end-=.1' )
+	// tl.add( slideOut(el.t2c), 'f2_end-=.2' )
+
 
 	tl.add('f3')
 	tl.set('.frame3', {opacity: 1}, 'f3')
-	tl.from('#t3a_', .5, {opacity:0})
+	tl.add( slideIn(el.t3a) )
+	tl.add( slideIn(el.t3b), '-=.2' )
+	tl.add( slideIn(el.t3c), '-=.2' )
 	
-	tl.add('f3_end', "+=.1")
+	tl.add('f3_end', "+=.5")
 	tl.from('#footer', .5, {opacity:0}, "f3_end")
 	tl.from('.proline', .5, {y:"+=90", opacity:0}, "f3_end")
 	tl.from('#cta', .5, {opacity:0}, '+=.3')
 
-	tl.set('#legalBtn', {display: "block"})	
+	tl.set('#legalBtn', {display: "block"})
+	
 }
 
 
