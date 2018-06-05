c79d0bd2c56b0ad97d735e05761ef9e0da535fed
Gar <gar@lonelydatu.com>
2018-06-05 11:08:43 -0400

    one

--- dev/passion_300x250/_bundled/log-free.js
+++ dev/passion_300x250/_bundled/log-free.js
@@ -76,9 +76,9 @@ var _commonJsCommonJs = require('../../_common/js/common.js');
 var el = {
 	t1a: { isLeft: false },
 	t1b: { isLeft: true },
-	t1c: { isLeft: false },
 	t2a: { isLeft: false },
 	t2b: { isLeft: true },
+	t2c: { isLeft: true },
 	t3a: { isLeft: true },
 	t3b: { isLeft: false },
 	t3c: { isLeft: false }
@@ -94,23 +94,21 @@ function start() {
 
 	tl.add((0, _commonJsCommonJs.slideIn)(el.t1a));
 	tl.add((0, _commonJsCommonJs.slideIn)(el.t1b), '-=.2');
-	tl.add((0, _commonJsCommonJs.slideIn)(el.t1c), '+=.1');
 
 	tl.add('f1_end', "+=1.5");
 	tl.add((0, _commonJsCommonJs.slideOut)(el.t1a), 'f1_end');
 	tl.add((0, _commonJsCommonJs.slideOut)(el.t1b), 'f1_end-=.1');
-	tl.add((0, _commonJsCommonJs.slideOut)(el.t1c), 'f1_end-=.2');
 
 	tl.add('f2');
 	tl.set('.frame2', { opacity: 1 }, 'f2');
 	tl.add((0, _commonJsCommonJs.slideIn)(el.t2a));
 	tl.add((0, _commonJsCommonJs.slideIn)(el.t2b), '-=.2');
-	// tl.add( slideIn(el.t2c), '-=.2' )
+	tl.add((0, _commonJsCommonJs.slideIn)(el.t2c), '-=.2');
 
 	tl.add('f2_end', "+=1.5");
 	tl.add((0, _commonJsCommonJs.slideOut)(el.t2a), 'f2_end');
 	tl.add((0, _commonJsCommonJs.slideOut)(el.t2b), 'f2_end-=.1');
-	// tl.add( slideOut(el.t2c), 'f2_end-=.2' )
+	tl.add((0, _commonJsCommonJs.slideOut)(el.t2c), 'f2_end-=.2');
 
 	tl.add('f3');
 	tl.set('.frame3', { opacity: 1 }, 'f3');
--- dev/passion_300x250/_js/main.js
+++ dev/passion_300x250/_js/main.js
@@ -3,10 +3,10 @@ import {size, slideIn, slideOut, init} from '../../_common/js/common.js'
 
 const el = {
 	t1a:{isLeft: false},
-	t1b:{isLeft: true},
-	t1c:{isLeft: false},
+	t1b:{isLeft: true},	
 	t2a:{isLeft: false},
 	t2b:{isLeft: true},	
+	t2c:{isLeft: true},
 	t3a:{isLeft: true},
 	t3b:{isLeft: false},
 	t3c:{isLeft: false}
@@ -27,23 +27,23 @@ function start() {
 
 	tl.add( slideIn(el.t1a) )
 	tl.add( slideIn(el.t1b), '-=.2' )
-	tl.add( slideIn(el.t1c), '+=.1' )
+	
 	
 	tl.add('f1_end', "+=1.5")
 	tl.add( slideOut(el.t1a), 'f1_end' )
 	tl.add( slideOut(el.t1b), 'f1_end-=.1' )
-	tl.add( slideOut(el.t1c), 'f1_end-=.2' )
+	
 
 	tl.add('f2')
 	tl.set('.frame2', {opacity: 1}, 'f2')
 	tl.add( slideIn(el.t2a) )
 	tl.add( slideIn(el.t2b), '-=.2' )
-	// tl.add( slideIn(el.t2c), '-=.2' )
+	tl.add( slideIn(el.t2c), '-=.2' )
 
 	tl.add('f2_end', "+=1.5")
 	tl.add( slideOut(el.t2a), 'f2_end' )
 	tl.add( slideOut(el.t2b), 'f2_end-=.1' )
-	// tl.add( slideOut(el.t2c), 'f2_end-=.2' )
+	tl.add( slideOut(el.t2c), 'f2_end-=.2' )
 
 
 	tl.add('f3')
